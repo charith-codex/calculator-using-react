6d620deb9bf76d933fac14f8b109e493b272959a
Charith Wickramasinghe <pvmcharith@gmail.com>
2024-01-25 13:07:36 +0530

    ver 1.2

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ function App() {
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand"></div>
-        <div className="current-operand"></div>
+        <div className="previous-operand">123,234 *</div>
+        <div className="current-operand">123, 234</div>
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
