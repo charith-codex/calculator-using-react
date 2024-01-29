2bfd017cb467ff19ac4db40a7903759b044b6046
Charith Wickramasinghe <pvmcharith@gmail.com>
2024-01-29 14:22:26 +0530

    ver 1.5

--- src/DigitButton.js
+++ src/DigitButton.js
@@ -1,5 +1,11 @@
-import {ACTIONS} from './App'
+import { ACTIONS } from "./App";
 
 export default function DigitButton({ dispatch, digit }) {
-    return <button onClick={()=> dispatch({type: ACTIONS.ADD_DIGIT, payload: { digit }})}>{digit}</button>;
+  return (
+    <button
+      onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+    >
+      {digit}
+    </button>
+  );
 }
